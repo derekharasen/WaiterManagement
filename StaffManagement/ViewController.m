754343fd335c8d3bd703ff6f448309e9400a27dc
Derek Harasen <Derek@Dereks-MacBook-Pro.local>
2015-03-14 18:53:57 -0400

    Initial commit

--- StaffManagement/ViewController.m
+++ StaffManagement/ViewController.m
@@ -7,15 +7,24 @@
 //
 
 #import "ViewController.h"
+#import "Restaurant.h"
+#import "RestaurantManager.h"
+#import "Waiter.h"
+
+static NSString * const kCellIdentifier = @"CellIdentifier";
 
 @interface ViewController ()
-
+@property IBOutlet UITableView *tableView;
+@property (nonatomic, retain) NSArray *waiters;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kCellIdentifier];
+    NSSortDescriptor *sortByName = [[NSSortDescriptor alloc]initWithKey:@"name" ascending:YES];
+    self.waiters = [[[RestaurantManager sharedManager]currentRestaurant].staff sortedArrayUsingDescriptors:@[sortByName]];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +32,17 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+#pragma mark - TableView Data Source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.waiters.count;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellIdentifier forIndexPath:indexPath];
+    Waiter *waiter = self.waiters[indexPath.row];
+    cell.textLabel.text = waiter.name;
+    return cell;
+}
 @end
