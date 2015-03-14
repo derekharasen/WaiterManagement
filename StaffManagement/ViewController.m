b8d350b5cc1dda5e0a99eae61d49279e1ff6df07
Derek Harasen <Derek@Dereks-MacBook-Pro.local>
2015-03-14 17:38:23 -0400

    Initial Commit

---
+++ StaffManagement/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StaffManagement
+//
+//  Created by Derek Harasen on 2015-03-14.
+//  Copyright (c) 2015 Derek Harasen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
