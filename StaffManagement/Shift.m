f0673e6e159ff37b04ab33609ec9a92aca9d2bc2
Kunwardeep Gill <kunwardeepgill@Kunwardeeps-MacBook-Pro.local>
2015-07-15 09:44:44 -0400

    shift model

---
+++ StaffManagement/Shift.m
@@ -0,0 +1,13 @@
+//
+//  Shift.m
+//  StaffManagement
+//
+//  Created by Kunwardeep Gill on 2015-07-15.
+//  Copyright (c) 2015 Derek Harasen. All rights reserved.
+//
+
+#import "Shift.h"
+
+@implementation Shift
+
+@end
