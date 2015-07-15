87c26632dc41add94540cf5ae0d0ca78690ccd91
Kunwardeep Gill <kunwardeepgill@Kunwardeeps-MacBook-Pro.local>
2015-07-15 10:16:58 -0400

    forgot the delegate

--- StaffManagement/NewWaiterViewController.m
+++ StaffManagement/NewWaiterViewController.m
@@ -23,6 +23,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.waiterNameTextField.delegate = self;
+
 }
 
 - (void)didReceiveMemoryWarning {
