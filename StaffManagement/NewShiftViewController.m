92769121a1fad18d1d61991c696c1b16354a0b64
Kunwardeep Gill <kunwardeepgill@Kunwardeeps-MacBook-Pro.local>
2015-07-15 18:51:18 -0400

    added Date Picker!

--- StaffManagement/NewShiftViewController.m
+++ StaffManagement/NewShiftViewController.m
@@ -14,6 +14,8 @@
 @interface NewShiftViewController () <UITextFieldDelegate>
 @property (strong, nonatomic) IBOutlet UITextField *startDateTextField;
 @property (strong, nonatomic) IBOutlet UITextField *endDateTextField;
+@property (strong, nonatomic) UIDatePicker *datePicker;
+
 
 @end
 
@@ -22,11 +24,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.startDateTextField.delegate = self;
+    self.endDateTextField.delegate = self;
+    
+    //  Initial datePicker when viewloaded
+    self.datePicker = [[UIDatePicker alloc]init];
+    [self.datePicker setDatePickerMode:UIDatePickerModeDateAndTime];
+    
+    self.startDateTextField.inputView = self.datePicker;
+    self.endDateTextField.inputView = self.datePicker;
+    
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"cccc, MMM d, hh:mm aa"];
+    
+    //  Checks to see if value changed.
+    [self.datePicker addTarget:self action:@selector(updateDateField:) forControlEvents:UIControlEventValueChanged];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//  hide the datepicker when anything else is touched
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    [self.startDateTextField resignFirstResponder];
+    [self.endDateTextField resignFirstResponder];
+}
+
+//  update the textfield with the date selected
+
+- (void)updateDateField:(id)sender
+{
+    
+    UIDatePicker *picker1 = (UIDatePicker *)self.startDateTextField.inputView;
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"cccc, MMM d, hh:mm aa"];
+    
+    if (self.startDateTextField.isFirstResponder) {
+        
+        self.startDateTextField.text = [dateFormat stringFromDate:picker1.date];
+        
+    } else {
+        self.endDateTextField.text = [dateFormat stringFromDate:picker1.date];
+        
+    }
 }
 
 #pragma mark - IBActions
@@ -42,8 +87,8 @@
     NSEntityDescription *shiftEntity = [NSEntityDescription entityForName:@"Shift" inManagedObjectContext:appDelegate.managedObjectContext];
     
     Shift *aShift = [[Shift alloc]initWithEntity:shiftEntity insertIntoManagedObjectContext:appDelegate.managedObjectContext];
-    aShift.startDate = self.startDateTextField.text;
-    aShift.endDate = self.endDateTextField.text;
+    aShift.startDate = [NSString stringWithFormat:@"Start: %@", self.startDateTextField.text];
+    aShift.endDate = [NSString stringWithFormat:@"End: %@", self.endDateTextField.text];
     Waiter *aWaiter;
     aWaiter = self.waiter;
     
