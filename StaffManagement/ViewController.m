74ad499e9559e8ef816cbd22bdc6af8677a5e72f
Stephen Gilroy <sgilroy@ualberta.ca>
2016-06-10 07:56:13 -0600

    Functional but requires cleaning up

--- StaffManagement/ViewController.m
+++ StaffManagement/ViewController.m
@@ -15,7 +15,7 @@ static NSString * const kCellIdentifier = @"CellIdentifier";
 
 @interface ViewController ()
 @property IBOutlet UITableView *tableView;
-@property (nonatomic, retain) NSArray *waiters;
+@property (nonatomic, retain) NSMutableArray *waiters;
 @end
 
 @implementation ViewController
@@ -24,7 +24,8 @@ static NSString * const kCellIdentifier = @"CellIdentifier";
     [super viewDidLoad];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kCellIdentifier];
     NSSortDescriptor *sortByName = [[NSSortDescriptor alloc]initWithKey:@"name" ascending:YES];
-    self.waiters = [[[RestaurantManager sharedManager]currentRestaurant].staff sortedArrayUsingDescriptors:@[sortByName]];
+    self.waiters = [[[[RestaurantManager sharedManager]currentRestaurant].staff sortedArrayUsingDescriptors:@[sortByName]] mutableCopy];
+    [self.tableView reloadData];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -32,6 +33,67 @@ static NSString * const kCellIdentifier = @"CellIdentifier";
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark - Add Waiter Button
+
+- (IBAction)addWaiterButton:(UIButton *)sender {
+    UIAlertView *alertViewAddName =[[UIAlertView alloc]initWithTitle:@"Add Waiter"
+                                                                message:@"What is the waiter's name?"
+                                                               delegate:self
+                                                      cancelButtonTitle:@"Cancel"
+                                                      otherButtonTitles:nil];
+    
+    alertViewAddName.alertViewStyle = UIAlertViewStylePlainTextInput;
+    [alertViewAddName show];
+}
+
+#pragma - Alert View
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    
+    NSManagedObjectContext *context = [self managedObjectContext];
+    
+    if ([[[alertView textFieldAtIndex:0] text] isEqualToString:@""]){
+        UIAlertView *emptyTextFieldAlertView =[[UIAlertView alloc]initWithTitle:@"Error"
+                                                                 message:@"No name was entered."
+                                                                delegate:self
+                                                       cancelButtonTitle:@"Okay"
+                                                       otherButtonTitles:nil];
+        [emptyTextFieldAlertView show];
+    }else{
+    
+    // Create a new managed object
+    NSEntityDescription *waiterEntity = [NSEntityDescription entityForName:@"Waiter" inManagedObjectContext:context];
+    Waiter *newWaiter = [[Waiter alloc]initWithEntity:waiterEntity insertIntoManagedObjectContext:context];
+    newWaiter.name = NSLocalizedString([[alertView textFieldAtIndex:0] text], nil);
+    [[[RestaurantManager sharedManager]currentRestaurant]  addStaffObject:newWaiter];
+
+    
+    NSError *error = nil;
+    // Save the object to persistent store
+    if (![context save:&error]) {
+        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
+    }
+    
+    // Fetch latest list from persistent store
+    
+    NSSortDescriptor *sortByName = [[NSSortDescriptor alloc]initWithKey:@"name" ascending:YES];
+    self.waiters = [[[[RestaurantManager sharedManager]currentRestaurant].staff sortedArrayUsingDescriptors:@[sortByName]] mutableCopy];
+    [self.tableView reloadData];
+    }
+}
+
+#pragma mark - Core Data
+
+- (NSManagedObjectContext *)managedObjectContext {
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+}
+
+
 #pragma mark - TableView Data Source
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
@@ -45,4 +107,30 @@ static NSString * const kCellIdentifier = @"CellIdentifier";
     cell.textLabel.text = waiter.name;
     return cell;
 }
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        if (editingStyle == UITableViewCellEditingStyleDelete) {
+            
+            NSManagedObjectContext *context = [self managedObjectContext];
+            
+            // Delete object from database
+            [context deleteObject:[self.waiters objectAtIndex:indexPath.row]];
+            
+            NSError *error = nil;
+            if (![context save:&error]) {
+                NSLog(@"Can't Delete! %@ %@", error, [error localizedDescription]);
+                return;
+            }
+            
+            // Remove device from table view
+            [self.waiters removeObjectAtIndex:indexPath.row];
+            [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        }
+    }
+}
+
 @end
