a23c36a73b6071a78176cc25033f11d5251afd0e
Jonester <chris.jones78@gmail.com>
2017-03-13 21:56:27 -0400

    Had to delete app off the simulator to remove linker errors

---
+++ Shift+CoreDataClass.m
@@ -0,0 +1,13 @@
+//
+//  Shift+CoreDataClass.m
+//  StaffManagement
+//
+//  Created by Chris Jones on 2017-03-13.
+//  Copyright © 2017 Derek Harasen. All rights reserved.
+//
+
+#import "Shift+CoreDataClass.h"
+#import "Waiter+CoreDataClass.h"
+@implementation Shift
+
+@end
---
+++ Shift+CoreDataProperties.h
@@ -0,0 +1,25 @@
+//
+//  Shift+CoreDataProperties.h
+//  StaffManagement
+//
+//  Created by Chris Jones on 2017-03-13.
+//  Copyright © 2017 Derek Harasen. All rights reserved.
+//
+
+#import "Shift+CoreDataClass.h"
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Shift (CoreDataProperties)
+
++ (NSFetchRequest<Shift *> *)fetchRequest;
+
+@property (nullable, nonatomic, copy) NSDate *date;
+@property (nullable, nonatomic, copy) NSDate *startTime;
+@property (nullable, nonatomic, copy) NSDate *endTime;
+@property (nullable, nonatomic, retain) Waiter *waiter;
+
+@end
+
+NS_ASSUME_NONNULL_END
