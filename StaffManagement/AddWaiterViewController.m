a638b7915116fad0de7682b024db586a2a232c49
Jonester <chris.jones78@gmail.com>
2017-03-13 20:07:51 -0400

    Set up prototype cell

--- StaffManagement/AddWaiterViewController.m
+++ StaffManagement/AddWaiterViewController.m
@@ -10,12 +10,16 @@
 #import "RestaurantManager.h"
 #import "AppDelegate.h"
 
+static NSString * const kCellIdentifier = @"CellIdentifier";
+
 @interface AddWaiterViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *addWaiterTextField;
 @property (weak, nonatomic) IBOutlet UIButton *addShiftOutlet;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UILabel *shiftsLabel;
+
+@property (nonatomic) NSMutableArray *shifts;
 
 @end
 
@@ -33,15 +37,13 @@
     self.addShiftOutlet.hidden = YES;
     self.shiftsLabel.hidden = YES;
     
-    
     if (self.waiter != nil) {
         [self setTitle:@"Edit Waiter"];
         self.addWaiterTextField.text = self.waiter.name;
         self.tableView.hidden = NO;
         self.addShiftOutlet.hidden = NO;
-        self.shiftsLabel.hidden = YES;
+        self.shiftsLabel.hidden = NO;
     }
-    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -82,5 +84,23 @@
 - (IBAction)addShift:(UIButton *)sender {
     
 }
+
+#pragma mark - TableView Data Source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.shifts.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellIdentifier forIndexPath:indexPath];
+    
+    return cell;
+}
+
+
+
 
 @end
